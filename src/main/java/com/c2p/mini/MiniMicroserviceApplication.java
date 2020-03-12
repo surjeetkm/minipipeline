8542ff770935afd5f0082600dcf8b58a7aefcd63
surjeetmohanty <surjeetmohanty2019@gmail.com>
2020-03-12 10:41:13 +0530

    first commit

---
+++ src/main/java/com/c2p/mini/MiniMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.c2p.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c2p/mini/MiniMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c2p.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
